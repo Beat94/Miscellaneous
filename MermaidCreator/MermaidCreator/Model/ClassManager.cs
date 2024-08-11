66f7417472ccea3efa9d73438c3322bd5a915438
Beat94 <36741096+Beat94@users.noreply.github.com>
2024-08-11 22:42:57 +0200

    ClassManager deleted mergeconflict

--- MermaidCreator/MermaidCreator/Model/ClassManager.cs
+++ MermaidCreator/MermaidCreator/Model/ClassManager.cs
@@ -5,12 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MermaidCreator.Model;
-
-<<<<<<< HEAD:MermaidCreator/MermaidCreator/Model/ClassManager.cs
-public class ClassMangaer
-=======
 public class ClassManager
->>>>>>> e091f3e327b74c8f2bd2fadd22a01af3bbb4b9a1:MermaidCreator/MermaidCreator/Model/ClassManaer.cs
 {
     IList<ClassConstructor> Classes = new List<ClassConstructor>();
     IList<ClassRelationship> ClassRelationships = new List<ClassRelationship>();
