2cd976af8a0b76065d29c71419072a7463c13550
Beat94 <36741096+Beat94@users.noreply.github.com>
2024-08-11 22:43:39 +0200

    deleted merge-conflict

--- MermaidCreator/MermaidCreator/Model/ClassVariable.cs
+++ MermaidCreator/MermaidCreator/Model/ClassVariable.cs
@@ -8,11 +8,7 @@ namespace MermaidCreator.Model;
 
 public class ClassVariable : Option
 {
-<<<<<<< HEAD
-
-=======
     public string getVariablename() => base.Name;
 
     public string getVariableWithAccessModifier() => base.getAccessModifier() + getVariablename();
->>>>>>> e091f3e327b74c8f2bd2fadd22a01af3bbb4b9a1
 }
