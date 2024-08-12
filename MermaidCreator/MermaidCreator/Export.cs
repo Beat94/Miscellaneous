d8114eee65f18cd4beb650c431b5060d43f5bbca
Beat94 <36741096+Beat94@users.noreply.github.com>
2024-08-12 23:29:21 +0200

    added Export.cs

---
+++ MermaidCreator/MermaidCreator/Export.cs
@@ -0,0 +1,29 @@
+﻿using MermaidCreator.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MermaidCreator;
+
+public class Export
+{
+    private void addRelation(string class1, string class2, ClassRelationship cr)
+    { 
+        
+    }
+
+    private void addClass()
+    { 
+    
+    }
+
+    public void CreateClassdiagram(ClassManager classManager)
+    {
+        string outputString = "classDiagram\n";
+
+
+    }
+
+}
