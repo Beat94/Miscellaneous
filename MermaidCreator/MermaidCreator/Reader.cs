94331884671b563e9824e72fdb4083a75a38b2a5
Beat94 <36741096+Beat94@users.noreply.github.com>
2024-09-01 23:54:29 +0200

    same same but different

--- MermaidCreator/MermaidCreator/Reader.cs
+++ MermaidCreator/MermaidCreator/Reader.cs
@@ -3,4 +3,8 @@
 public class Reader
 {
     // goes through project and creates mermaid model using Mermaid Model-Class
+
+    // Filters all files in folder with the ending .cs
+
+    // Opens file and analizes it
 }
