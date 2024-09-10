2872bf7ef458b7984db9aff351edda1bbdfcdd78
Beat94 <36741096+Beat94@users.noreply.github.com>
2024-09-10 21:44:43 +0200

    minor change in unittest

--- MermaidCreator/MermaidCreatorTest/UnitTest1.cs
+++ MermaidCreator/MermaidCreatorTest/UnitTest1.cs
@@ -1,12 +1,11 @@
-namespace MermaidCreatorTets
-{
-    public class UnitTest1
-    {
-        [Theory]
-        [InlineData("Testcase1Input.cs.txt", "Testcase1Output.json")]
-        public void Test1(string input, string output)
-        {
+namespace MermaidCreatorTets;
 
-        }
+public class UnitTest1
+{
+    [Theory]
+    [InlineData("Testcase1Input.cs.txt", "Testcase1Output.json")]
+    public void Test1(string input, string output)
+    {
+
     }
 }
