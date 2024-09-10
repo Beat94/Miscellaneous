ad6b5eac6e4943621d8398dc938954a4012d9f1e
Beat94 <36741096+Beat94@users.noreply.github.com>
2024-09-10 18:59:37 +0200

    added Testfile

---
+++ MermaidCreator/MermaidCreatorTest/ReaderTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MermaidCreatorTest;
+
+public class ReaderTest
+{
+    [Fact]
+    public void testli()
+    { 
+        
+    }
+}
