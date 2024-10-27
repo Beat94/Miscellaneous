6a9f4f4d04ee8c698f3032957afd067115c33ae7
Beat94 <36741096+Beat94@users.noreply.github.com>
2024-10-27 21:59:28 +0100

    created selfreflection-function WIP

--- MermaidCreator/MermaidCreator/ReflectionHelper.cs
+++ MermaidCreator/MermaidCreator/ReflectionHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 using MermaidCreator.Model;
 
 namespace MermaidCreator;
@@ -17,6 +18,11 @@ internal class ReflectionHelper
     /// </summary>
     internal ClassManager SelfReflection()
     {
+        // https://medium.com/technology-nineleaps/reflection-in-net-a-comprehensive-guide-for-application-development-66841aa6f4b1
+        Assembly CurrentAssembly = Assembly.GetExecutingAssembly();
+
+
+
         return new ClassManager();
     }
 }
