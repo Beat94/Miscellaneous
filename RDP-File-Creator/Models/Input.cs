65f1500afe21a379823795a02ee8aa5a75ea0ffb
Beat94 <36741096+Beat94@users.noreply.github.com>
2024-12-30 08:06:11 +0100

    Beat94/rdp file creator (#17)
    * init RDP-File-Creator
    
    * ..
    
    ---------
    
    Co-authored-by: Rey Beat Giuliano (CH/IUB) <beatgiuliano.rey@swisslife.ch>

---
+++ RDP-File-Creator/Models/Input.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDP_File_Creator.Models;
+public class Input
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string[] ServerNames { get; set; }
+}
---
+++ RDP-File-Creator/Program.cs
@@ -0,0 +1,44 @@
+﻿using RDP_File_Creator.Models;
+using RdpConnection;
+using System.Text;
+using System.Text.Json;
+
+namespace RDP_File_Creator;
+class Program
+{ 
+    public static void Main()
+    {
+        string inputJsonLocation = ".\\Input-Files\\input.json";
+        string outputRdpLocation = "C:\\Users\\[Username]";
+
+        Console.WriteLine($"Input-File: {inputJsonLocation}");
+        Console.WriteLine($"Output-Location: {outputRdpLocation}");
+
+        Input input;
+
+        try 
+        {
+            input = JsonSerializer.Deserialize<Input>(File.ReadAllText(inputJsonLocation));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+            Console.WriteLine($"Error-Data: {ex.Data}");
+            return;
+        }
+
+        foreach (string server in input.ServerNames)
+        {
+            var rdp = new MicrosoftTerminalServiceClientProperties()
+            {
+                FullAddress = server,
+                ScreenModeId = 2, // 1: window mode, 2: full screen
+                Username = input.Username
+            };
+
+            rdp.SetPassword(input.Password);
+
+            File.WriteAllText(@$"{outputRdpLocation}\{server}.rdp", string.Join(Environment.NewLine, rdp.Serialize()), new UTF8Encoding(false));
+        }
+    }
+}
