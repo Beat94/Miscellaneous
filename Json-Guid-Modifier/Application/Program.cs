783c39eec6394715ba8c739222390658eaf57fd9
Beat94 <36741096+Beat94@users.noreply.github.com>
2025-01-22 20:30:27 +0100

    added File loading

--- Json-Guid-Modifier/Application/Program.cs
+++ Json-Guid-Modifier/Application/Program.cs
@@ -1,14 +1,16 @@
 ﻿using Newtonsoft.Json.Linq;
+using System.Text.Json;
 
 namespace Application;
 
 internal class Program
 {
     internal static void Main()
-    { 
-        Sys1 sys = new Sys1();
-
-        sys.SetNewGuids(new JObject(), ["asdf", "asdf"]);
+    {
+        string link = "Test.json";
+        string json = System.IO.File.ReadAllText(link);
+        Sys1 sys = new();
+        JObject output = sys.SetNewGuids(JObject.Parse(json), ["asdf", "asdf"]);
 
         Console.WriteLine("Hello, World 2!");
     }
