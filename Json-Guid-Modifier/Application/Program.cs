008cc00b6e9ad034c26d06f34df7cd092c6f96f3
Beat94 <36741096+Beat94@users.noreply.github.com>
2025-01-23 09:28:45 +0100

    Beat94/json g UI d modifier (#19)
    * created new Solution for Json UG
    
    * updated csproj and sln
    
    * added new class
    
    * added interface
    
    * some changes
    
    * finalized idea?
    
    * added File loading
    
    ---------
    
    Co-authored-by: Beat Giuliano Rey <beatgiuliano.rey@swisslife.ch>

---
+++ Json-Guid-Modifier/Application/Program.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Text.Json;
+
+namespace Application;
+
+internal class Program
+{
+    internal static void Main()
+    {
+        string link = "Test.json";
+        string json = System.IO.File.ReadAllText(link);
+        Sys1 sys = new();
+        JObject output = sys.SetNewGuids(JObject.Parse(json), ["asdf", "asdf"]);
+
+        Console.WriteLine("Hello, World 2!");
+    }
+}
---
+++ Json-Guid-Modifier/Application/Sys1.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Application;
+
+public class Sys1 : IJsonSystem
+{
+    public JObject SetNewGuids(JObject input, string[] valuesToChange)
+    {
+        // should go through valuesToChange and set new Guids
+
+        try
+        {
+            foreach (string value in valuesToChange)
+            {
+                input[value] = Guid.NewGuid().ToString();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+
+        return input;
+    }
+}
