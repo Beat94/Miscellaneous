5670ed75921f855e1317b0bceac26d58d2f1787b
Beat94 <36741096+Beat94@users.noreply.github.com>
2025-03-03 20:38:14 +0100

    added functions and datamodel

--- PDFBase64/Base64Trafo/Form1.Designer.cs
+++ PDFBase64/Base64Trafo/Form1.Designer.cs
@@ -30,11 +30,16 @@
         {
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
+            button3 = new Button();
+            FileLocation = new TextBox();
             label1 = new Label();
             tabPage2 = new TabPage();
+            label3 = new Label();
+            comboBox1 = new ComboBox();
+            label2 = new Label();
+            Base64Input = new TextBox();
             button1 = new Button();
             button2 = new Button();
-            Base64Input = new TextBox();
             tabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
             tabPage2.SuspendLayout();
@@ -44,84 +49,128 @@
             // 
             tabControl1.Controls.Add(tabPage1);
             tabControl1.Controls.Add(tabPage2);
-            tabControl1.Location = new Point(21, 24);
-            tabControl1.Margin = new Padding(5, 6, 5, 6);
+            tabControl1.Location = new Point(12, 12);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(1330, 758);
+            tabControl1.Size = new Size(776, 379);
             tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
+            tabPage1.Controls.Add(button3);
+            tabPage1.Controls.Add(FileLocation);
             tabPage1.Controls.Add(label1);
-            tabPage1.Location = new Point(4, 39);
-            tabPage1.Margin = new Padding(5, 6, 5, 6);
+            tabPage1.Location = new Point(4, 24);
             tabPage1.Name = "tabPage1";
-            tabPage1.Padding = new Padding(5, 6, 5, 6);
-            tabPage1.Size = new Size(1322, 715);
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(768, 351);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "File -> Base64";
             tabPage1.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            button3.Location = new Point(668, 21);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 2;
+            button3.Text = "Open";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += OpenFileToEncrypt;
+            // 
+            // FileLocation
+            // 
+            FileLocation.Location = new Point(124, 22);
+            FileLocation.Margin = new Padding(2);
+            FileLocation.Name = "FileLocation";
+            FileLocation.Size = new Size(522, 23);
+            FileLocation.TabIndex = 1;
+            // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(55, 50);
-            label1.Margin = new Padding(5, 0, 5, 0);
+            label1.Location = new Point(32, 25);
             label1.Name = "label1";
-            label1.Size = new Size(123, 30);
+            label1.Size = new Size(79, 15);
             label1.TabIndex = 0;
-            label1.Text = "Speicherort:";
+            label1.Text = "File-Location:";
             // 
             // tabPage2
             // 
+            tabPage2.Controls.Add(label3);
+            tabPage2.Controls.Add(comboBox1);
+            tabPage2.Controls.Add(label2);
             tabPage2.Controls.Add(Base64Input);
-            tabPage2.Location = new Point(4, 39);
-            tabPage2.Margin = new Padding(5, 6, 5, 6);
+            tabPage2.Location = new Point(4, 24);
             tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(5, 6, 5, 6);
-            tabPage2.Size = new Size(1322, 715);
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(768, 351);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "Base64 -> File";
             tabPage2.UseVisualStyleBackColor = true;
             // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(18, 73);
+            label3.Name = "label3";
+            label3.Size = new Size(51, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Filetype:";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(120, 70);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(233, 23);
+            comboBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(18, 28);
+            label2.Name = "label2";
+            label2.Size = new Size(79, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Base64-Code:";
+            // 
+            // Base64Input
+            // 
+            Base64Input.Location = new Point(120, 25);
+            Base64Input.Name = "Base64Input";
+            Base64Input.Size = new Size(594, 23);
+            Base64Input.TabIndex = 0;
+            // 
             // button1
             // 
-            button1.Location = new Point(1215, 812);
-            button1.Margin = new Padding(5, 6, 5, 6);
+            button1.Location = new Point(709, 406);
             button1.Name = "button1";
-            button1.Size = new Size(129, 46);
+            button1.Size = new Size(75, 23);
             button1.TabIndex = 1;
             button1.Text = "Close";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += CloseFunction;
             // 
             // button2
             // 
             button2.AccessibleName = "ToClipboard";
-            button2.Location = new Point(1063, 812);
-            button2.Margin = new Padding(5, 6, 5, 6);
+            button2.Location = new Point(620, 406);
             button2.Name = "button2";
-            button2.Size = new Size(142, 46);
+            button2.Size = new Size(83, 23);
             button2.TabIndex = 0;
             button2.Text = "To Clipboard";
             button2.UseVisualStyleBackColor = true;
-            // 
-            // Base64Input
-            // 
-            Base64Input.Location = new Point(154, 47);
-            Base64Input.Name = "Base64Input";
-            Base64Input.Size = new Size(1060, 35);
-            Base64Input.TabIndex = 0;
+            button2.Click += ToClipboard;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1371, 900);
+            ClientSize = new Size(800, 450);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(tabControl1);
-            Margin = new Padding(5, 6, 5, 6);
             Name = "Form1";
             Text = "Base64 Transfomer";
             tabControl1.ResumeLayout(false);
@@ -130,6 +179,8 @@
             tabPage2.ResumeLayout(false);
             tabPage2.PerformLayout();
             ResumeLayout(false);
+            LoadFileTypeModel();
+            FillComboBox();
         }
 
         #endregion
@@ -140,6 +191,11 @@
         private Button button1;
         private Button button2;
         private Label label1;
+        private Button button3;
+        private TextBox FileLocation;
+        private Label label2;
         private TextBox Base64Input;
+        private Label label3;
+        private ComboBox comboBox1;
     }
 }
--- PDFBase64/Base64Trafo/GuiFunctions.cs
+++ PDFBase64/Base64Trafo/GuiFunctions.cs
@@ -1,15 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Base64Trafo.DataModels;
+using System.Text.Json;
 
 namespace Base64Trafo;
 
-internal class GuiFunctions : Form1
+public partial class Form1
 {
-    internal void ToClipboard(object sender, EventArgs e)
-    { 
-    
+    FileType fileTypeModel;
+    string filepath;
+    //var fileStream;
+
+    #region UI-Wide Functions
+    void CloseFunction(object sender, EventArgs e)
+    {
+        Application.Exit();
     }
+    void ToClipboard(object sender, EventArgs e)
+    {
+        if (tabControl1.SelectedIndex == 0)
+        {
+            // File -> Base64
+            Clipboard.SetText("Index 1");
+        }
+        else
+        {
+            // Base64 -> File
+            Clipboard.SetText("Index 2");
+        }
+
+    }
+    #endregion
+
+    #region Startup Functions
+    void LoadFileTypeModel()
+    {
+        this.fileTypeModel = JsonSerializer.Deserialize<FileType>(File.ReadAllText(".\\Data\\filetype.json"));
+    }
+    void FillComboBox()
+    {
+
+        List<string> filetypesNameList = new();
+        filetypesNameList.Add(" ");
+
+        foreach (Filetypes filetyp in this.fileTypeModel.Filetypes)
+        {
+            filetypesNameList.Add(filetyp.Name);
+        }
+
+        comboBox1.DataSource = filetypesNameList;
+    }
+    #endregion
+
+    #region Tab1 Functions
+    public void OpenFileToEncrypt(object sender, EventArgs e)
+    {
+        using (OpenFileDialog openFileDialog = new())
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filepath = openFileDialog.FileName;
+            }
+        }
+
+        FileLocation.Text = filepath;
+    }
+    #endregion
 }
