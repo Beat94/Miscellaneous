bd3fe3b017ca2fa6350af8176f40a0051612dc81
Beat Giuliano Rey <beat_g_94@hotmail.com>
2025-03-03 07:41:23 +0100

    added function

--- PDFBase64/Base64Trafo/Form1.Designer.cs
+++ PDFBase64/Base64Trafo/Form1.Designer.cs
@@ -34,27 +34,31 @@
             tabPage2 = new TabPage();
             button1 = new Button();
             button2 = new Button();
+            Base64Input = new TextBox();
             tabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
             SuspendLayout();
             // 
             // tabControl1
             // 
             tabControl1.Controls.Add(tabPage1);
             tabControl1.Controls.Add(tabPage2);
-            tabControl1.Location = new Point(12, 12);
+            tabControl1.Location = new Point(21, 24);
+            tabControl1.Margin = new Padding(5, 6, 5, 6);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(776, 379);
+            tabControl1.Size = new Size(1330, 758);
             tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
             tabPage1.Controls.Add(label1);
-            tabPage1.Location = new Point(4, 24);
+            tabPage1.Location = new Point(4, 39);
+            tabPage1.Margin = new Padding(5, 6, 5, 6);
             tabPage1.Name = "tabPage1";
-            tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(768, 351);
+            tabPage1.Padding = new Padding(5, 6, 5, 6);
+            tabPage1.Size = new Size(1322, 715);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "File -> Base64";
             tabPage1.UseVisualStyleBackColor = true;
@@ -62,27 +66,31 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(32, 25);
+            label1.Location = new Point(55, 50);
+            label1.Margin = new Padding(5, 0, 5, 0);
             label1.Name = "label1";
-            label1.Size = new Size(70, 15);
+            label1.Size = new Size(123, 30);
             label1.TabIndex = 0;
             label1.Text = "Speicherort:";
             // 
             // tabPage2
             // 
-            tabPage2.Location = new Point(4, 24);
+            tabPage2.Controls.Add(Base64Input);
+            tabPage2.Location = new Point(4, 39);
+            tabPage2.Margin = new Padding(5, 6, 5, 6);
             tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(768, 351);
+            tabPage2.Padding = new Padding(5, 6, 5, 6);
+            tabPage2.Size = new Size(1322, 715);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "Base64 -> File";
             tabPage2.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            button1.Location = new Point(709, 406);
+            button1.Location = new Point(1215, 812);
+            button1.Margin = new Padding(5, 6, 5, 6);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(129, 46);
             button1.TabIndex = 1;
             button1.Text = "Close";
             button1.UseVisualStyleBackColor = true;
@@ -90,26 +98,37 @@
             // button2
             // 
             button2.AccessibleName = "ToClipboard";
-            button2.Location = new Point(620, 406);
+            button2.Location = new Point(1063, 812);
+            button2.Margin = new Padding(5, 6, 5, 6);
             button2.Name = "button2";
-            button2.Size = new Size(83, 23);
+            button2.Size = new Size(142, 46);
             button2.TabIndex = 0;
             button2.Text = "To Clipboard";
             button2.UseVisualStyleBackColor = true;
             // 
+            // Base64Input
+            // 
+            Base64Input.Location = new Point(154, 47);
+            Base64Input.Name = "Base64Input";
+            Base64Input.Size = new Size(1060, 35);
+            Base64Input.TabIndex = 0;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(12F, 30F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1371, 900);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(tabControl1);
+            Margin = new Padding(5, 6, 5, 6);
             Name = "Form1";
             Text = "Base64 Transfomer";
             tabControl1.ResumeLayout(false);
             tabPage1.ResumeLayout(false);
             tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -121,5 +140,6 @@
         private Button button1;
         private Button button2;
         private Label label1;
+        private TextBox Base64Input;
     }
 }
---
+++ PDFBase64/Base64Trafo/GuiFunctions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base64Trafo;
+
+internal class GuiFunctions : Form1
+{
+    internal void ToClipboard(object sender, EventArgs e)
+    { 
+    
+    }
+}
