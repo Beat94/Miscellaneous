265d935252d5c1a69f7ee469d673900aa218d2d2
Beat94 <36741096+Beat94@users.noreply.github.com>
2025-03-04 21:35:42 +0100

    enhanced

--- PDFBase64/Base64Trafo/GuiFunctions.cs
+++ PDFBase64/Base64Trafo/GuiFunctions.cs
@@ -1,12 +1,16 @@
 ﻿using Base64Trafo.DataModels;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
 using System.Text.Json;
+using System.Windows.Forms.VisualStyles;
 
 namespace Base64Trafo;
 
 public partial class Form1
 {
     FileType fileTypeModel;
-    string filepath;
+    string filepath = string.Empty;
+    string openFileDialogFilter = string.Empty;
 
     #region UI-Wide Functions
     void CloseFunction(object sender, EventArgs e)
@@ -19,13 +23,57 @@ public partial class Form1
         {
             // File -> Base64
             Clipboard.SetText("Index 1");
+
+            string path = string.Empty;
+
+            // Value loading from interface
+            if (!string.IsNullOrEmpty(filepath) || !string.IsNullOrEmpty(FileLocation.Text))
+            {
+                path = FileLocation.Text ?? filepath;
+            }
+            else
+            {
+                MessageBox.Show($"{nameof(filepath)} and Inputbox are empty, please choose file");
+                return;
+            }
+
+            // check if path to document and documentending is in
+            // filetype.json - if not throw 
+            bool isCorrectFiletype = false;
+
+            string FileEndingUIFunc(string? x = null)
+            {
+                List<string> xList = new();
+                xList = x.Split('.').ToList();
+                return $".{xList.ElementAt(xList.Count - 1)}";
+            }
+
+            string fileedingUI = "." + FileEndingUIFunc(path);
+
+            foreach (Filetypes filetypes in fileTypeModel.Filetypes)
+            {
+                string fileendingJson = filetypes.Fileending.TrimStart('*');
+
+                if (fileedingUI.Equals(fileendingJson))
+                {
+                    isCorrectFiletype = true;
+                }
+            }
+
+            if (isCorrectFiletype == false)
+            {
+                MessageBox.Show("Invalid filetype.");
+                return;
+            }
+
+            // load Base64 encoded string to clipboard
+
         }
         else
         {
             // Base64 -> File
             Clipboard.SetText("Index 2");
         }
-
     }
     #endregion
 
@@ -33,6 +81,17 @@ public partial class Form1
     void LoadFileTypeModel()
     {
         this.fileTypeModel = JsonSerializer.Deserialize<FileType>(File.ReadAllText(".\\Data\\filetype.json"));
+        List<Filetypes> fileTypesList = fileTypeModel.Filetypes.ToList();
+
+        foreach (Filetypes fileTypes in fileTypesList)
+        {
+            if (!string.IsNullOrEmpty(this.openFileDialogFilter))
+            {
+                this.openFileDialogFilter += "|";
+            }
+
+            this.openFileDialogFilter += $"{fileTypes.Name} ({fileTypes.Fileending})|{fileTypes.Fileending}";
+        }
     }
     void FillComboBox()
     {
@@ -54,6 +113,9 @@ public partial class Form1
     {
         using (OpenFileDialog openFileDialog = new())
         {
+            openFileDialog.Filter = openFileDialogFilter;
+            //openFileDialog.FilterIndex = 2;
+
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
                 filepath = openFileDialog.FileName;
