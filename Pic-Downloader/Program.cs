13380f06be8475aa33acd29420d7f738c15706c2
Beat94 <36741096+Beat94@users.noreply.github.com>
2025-05-01 23:23:27 +0200

    Beat94/pic downloader (#30)
    * added Pic-Downloader
    
    * readme
    
    * final change

---
+++ Pic-Downloader/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace picDownloader;
+
+public class Program
+{
+    public static void Main()
+    {
+        Runner run = new();
+        run.savepath = ""; // Path of file including linklist
+        run.datapath = ""; // Path of savinglocation
+        run.cookie = "";
+
+        run.running();
+    }
+}
---
+++ Pic-Downloader/Readme.md
@@ -0,0 +1,10 @@
+# Introduction
+It is a simple picture downloader, which goes through the linklist and downloads pictures.
+
+# Linklist
+Linklist should contain one link on one line.
+Example:
+```
+www.google.ch
+www.yahoo.com
+```
