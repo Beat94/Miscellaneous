b9938108f53ff8e2594d28d724fd30cbf1cdc056
Beat94 <36741096+Beat94@users.noreply.github.com>
2025-12-19 15:20:48 +0100

    Update Program.cs

--- MarkdownTOCCreator/MarkDownHelperProgram/Program.cs
+++ MarkdownTOCCreator/MarkDownHelperProgram/Program.cs
@@ -3,7 +3,7 @@
     public static void Main(string[]args)
     {
         bool isTesting = false;
-        string linkToMarkdown = "C:\\Users\\reyb\\OneDrive - Swiss Life AG\\H Laufwerk\\Dokumente\\Projekte\\2025\\Christine-Fach\\Support-Fach\\Support-Fach.md";
+        string linkToMarkdown = "C:\\Users\\Support-Fach.md";
 
         MarkdownHelper markdownHelper = new(isTesting);
 
